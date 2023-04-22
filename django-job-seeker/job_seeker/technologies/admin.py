ad0a7c29469cf4f5a87b9d316d88f8461c04d8b2
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-04-21 22:54:06 -0600

    Job Seeker: New model 'technologies'

--- django-job-seeker/job_seeker/technologies/admin.py
+++ django-job-seeker/job_seeker/technologies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from technologies.models import Technology
+
+
 # Register your models here.
+
+
+admin.site.register(Technology)
--- django-job-seeker/job_seeker/technologies/models.py
+++ django-job-seeker/job_seeker/technologies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from common.models import Base
+
 # Create your models here.
+
+
+class Technology(Base):
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "technologies"
+
+    def __str__(self) -> str:
+        return self.name
