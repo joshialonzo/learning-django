58e90ffb0fa5a25af830d17e81c3bb8190d644a1
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-04-21 20:14:24 -0600

    Job Seeker: Move model admin's from 'common' to 'companies'

--- django-job-seeker/job_seeker/common/admin.py
+++ django-job-seeker/job_seeker/common/admin.py
@@ -1,23 +1,4 @@
 from django.contrib import admin
 
-from companies.models.company import Company
-from companies.models.job_type import JobType
-
 
 # Register your models here.
-
-class JobTypeAdmin(admin.ModelAdmin):
-    list_display = ("name", "created_date", "updated_date", "id")
-
-
-admin.site.register(JobType, JobTypeAdmin)
-
-
-class CompanyAdmin(admin.ModelAdmin):
-    list_display = (
-        "name", "company_url",
-        "linkedin_id", "country", "id",
-    )
-
-
-admin.site.register(Company, CompanyAdmin)
