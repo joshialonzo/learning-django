72161260bf40fed021266e7a38f541d809fd3bf9
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-05-02 17:26:38 -0600

    Micro-ecommerce: Product Create View & Template

--- micro-ecommerce/src/products/views.py
+++ micro-ecommerce/src/products/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from .forms import ProductForm
+from .models import Product
+
+
+def product_create_view(request):
+    context = {}
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid():
+        obj = form.save(commit=False)
+        if request.user.is_authenticated:
+            obj.user = request.user
+            obj.save()
+            return redirect("/products/create/")
+        else:
+            form.add_error(None, "You must be logged in to create products")
+
+    context["form"] = form
+    return render(request, "products/create.html", context)
