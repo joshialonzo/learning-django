e78e07ae3af993acb8e9138c59e288f708758b6a
Josue Alonzo-Chavarria <joshi.alonzo@gmail.com>
2023-05-02 21:25:42 -0600

    Micro-ecommerce: Product Detail & Update View

--- micro-ecommerce/src/products/views.py
+++ micro-ecommerce/src/products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect, render
 
 # Create your views here.
 from .forms import ProductForm
@@ -16,7 +17,10 @@ def product_create_view(request):
             obj.save()
             return redirect("/products/create/")
         else:
-            form.add_error(None, "You must be logged in to create products")
+            form.add_error(
+                None,
+                "You must be logged in to create products",
+            )
 
     context["form"] = form
     return render(request, "products/create.html", context)
@@ -28,3 +32,23 @@ def product_list_view(request):
         "object_list": object_list,
     }
     return render(request, "products/list.html", context)
+
+
+def product_detail_view(request, handle=None):
+    object = get_object_or_404(Product, handle=handle)
+    is_owner = (
+        object.user == request.user
+        if request.user.is_authenticated
+        else False
+    )
+    context = {"object": object}
+    if is_owner:
+        form = ProductForm(
+            request.POST or None,
+            instance=object,
+        )
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.save()
+        context["form"] = form
+    return render(request, "products/detail.html", context)
